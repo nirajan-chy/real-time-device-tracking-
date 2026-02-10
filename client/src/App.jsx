fc3aaa74ed3374a96c2edec0bd13336010b6fc25
nirajan-chy <nirajanchaudhary00001@gmail.com>
2026-02-10 17:23:10 +0545

    feat:home

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import UserLogin from "./pages/UserLogin";
+import UserSignup from "./pages/UserSignup";
+import CaptainLogin from "./pages/CaptainLogin";
+import CaptainSignup from "./pages/CaptainSignup";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserSignup />} />
+        <Route path="/Captain-signup" element={<CaptainSignup />} />
+        <Route path="/captain-login" element={<CaptainLogin />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="h-screen bg-cover bg-center bg-[url(/public/background.png)] pt-5  flex justify-between flex-col w-full bg-red-400">
+        <img className="w-30 ml-5  " src="/public/logo.png" alt="" />
+        <div className="bg-white py-4 px-4 pb-7">
+          <h2 classN ame="text-3xl font-bold">Get Started with Uber</h2>
+          <Link
+            to="/login"
+            className="flex items-center justify-center w-full  bg-black text-white py-3 rounded mt-5"
+          >
+            Continue
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
